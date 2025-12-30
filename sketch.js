4046bfa46e1eab7d72ed523469a780eb97215a02
esemv07 <ruby.semaganda@gmail.com>
2025-12-31 07:32:59 +1100

    Pause motion when changing slider

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
 let sun;
 let mercury;
+let venus;
+let earth;
+let mars;
+let jupiter;
+let saturn;
+let uranus;
+let neptune;
 let G = (4.03 * Math.pow(10, -5)); // 1 second = 1 week
 let zoom = 0.5;
+let animationPaused = false;
 
 
 function setup() {
 	createCanvas((windowWidth-400), (windowHeight-25));
+
 	sun = new Body(333000, createVector(0, 0), createVector(0, 0), 34.817, color(253, 204, 108));
 
 
@@ -30,53 +39,65 @@ function setup() {
 	title.position(width+10, 0);
 
 	grav = createSlider((1 * Math.pow(10, -5)), (1 * Math.pow(10, -4)), (4.03 * Math.pow(10, -5)), (1 * Math.pow(10, -7)));
-	grav.position(width+10, 50);
+	grav.position(width+10, 60);
 	grav.size(200);
+	grav.input(pauseOrbit);
+	grav.changed(resumeOrbit);
 
-	grav_label = createP(`Universal Gravitational Constant: ${G}`);
+	grav_label = createP(`Universal Gravitational Constant: ${G*100000}x10<sup>-5<sup>`);
 	grav_label.position(width+10, 70)
 }
 
 function draw() {
 	G = grav.value();
 
-	grav_label.html(`Universal Gravitational Constant: ${G}`)
+	grav_label.html(`Universal Gravitational Constant: ${G*100000}x10<sup>-5<sup>`)
 
 	translate(width/2, height/2);
 	background(40);
 
 	scale(zoom);
 
-	sun.attract(mercury);
-	mercury.update();
+	if (!animationPaused) {
+		sun.attract(mercury);
+		mercury.update();
+
+		
+		sun.attract(venus);
+		venus.update();
+		
+
+		sun.attract(earth);
+		earth.update();
+		
+
+		sun.attract(mars);
+		mars.update();
+		
+
+		sun.attract(jupiter);
+		jupiter.update();
+		
+
+		sun.attract(saturn);
+		saturn.update();
+		
+
+		sun.attract(uranus);
+		uranus.update();
+		
+
+		sun.attract(neptune);
+		neptune.update();
+	}
+
 	mercury.show();
-	
-	sun.attract(venus);
-	venus.update();
 	venus.show();
-
-	sun.attract(earth);
-	earth.update();
 	earth.show();
-
-	sun.attract(mars);
-	mars.update();
 	mars.show();
-
-	sun.attract(jupiter);
-	jupiter.update();
 	jupiter.show();
-
-	sun.attract(saturn);
-	saturn.update();
 	saturn.show();
-
-	sun.attract(uranus);
-	uranus.update();
 	uranus.show();
-
-	sun.attract(neptune);
-	neptune.update();
 	neptune.show();
 
 	sun.show();
@@ -106,6 +127,14 @@ function planetVel(planetPos) {
 	planetVel.rotate(HALF_PI);
 	planetVel.setMag( sqrt( G * sun.mass / planetPos.mag() ) )
 	return planetVel;
+}
+
+function pauseOrbit() {
+	animationPaused = true;
+}
+
+function resumeOrbit() {
+	animationPaused = false;
 }
 
 function Body(mass, pos, vel, radius, colour) {
